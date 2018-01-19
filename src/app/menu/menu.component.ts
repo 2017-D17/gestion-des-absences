b0d7d2b0263b7b43ec8a4e0e5fb9c944e3363b61
Asia Trabelsi <assia.trabelsi93@gmail.com>
2018-01-19 10:25:30 +0100

    menu-manager

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+import { Stream } from "stream";
+
+@Component({
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
+})
+export class MenuComponent implements OnInit {
+  name: String;
+  constructor() {}
+
+  ngOnInit() {}
+}
