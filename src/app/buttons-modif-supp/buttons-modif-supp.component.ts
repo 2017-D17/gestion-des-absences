97d0313bc1d51033424fc0a202c7a8698b6c1384
Sandra LE THIEC <lethiec.sandra@gmail.com>
2018-01-24 13:30:58 +0100

    IT1 - fix absence rejetee peut etre modifiee

--- src/app/buttons-modif-supp/buttons-modif-supp.component.ts
+++ src/app/buttons-modif-supp/buttons-modif-supp.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from "@angular/core";
 import { Absence } from "../shared/domain/absence";
 import { AbsenceService } from "../shared/service/absence.service";
 import { FerieType, FERIE_TYPES } from '../shared/domain/ferie-type.enum';
+import { AbsenceStatut, ABSENCES_STATUS } from "../absence-statut.enum";
 
 @Component({
   selector: "app-buttons-modif-supp",
@@ -20,10 +21,10 @@ export class ButtonsModifSuppComponent implements OnInit {
   constructor(private aService: AbsenceService) {}
 
   ngOnInit() {
-    if (this.absence.statut == "INITIALE" && this.absence.type != FerieType.RTT_EMPLOYEUR) {
+    if ((this.absence.statut == AbsenceStatut.INITIALE || this.absence.statut == AbsenceStatut.REJETEE )&& this.absence.type != FerieType.RTT_EMPLOYEUR) {
       this.modifAbsence = "update";
       this.aff = true;
-    } else if(this.absence.statut == "EN_ATTENTE_VALIDATION" ) {
+    } else if(this.absence.statut == AbsenceStatut.EN_ATTENTE_VALIDATION ) {
       this.aff = true;
     }
   }
