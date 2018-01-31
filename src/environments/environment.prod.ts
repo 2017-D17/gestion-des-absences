34a6a01859d6771aff23bcb5aafc2c9f30ab8ac7
Sandra LE THIEC <lethiec.sandra@gmail.com>
2018-01-31 09:40:29 +0100

    suppression page admin component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlBackEndAbsences: 'https://gestion-des-absences.herokuapp.com/absences/',
-  urlBackEndAbsencesStatut: 'http://gestion-des-absences.herokuapp.com/absences?statut=',
-  urlBackEndJoursFeries: 'https://gestion-des-absences.herokuapp.com/jours_feries/',
-  urlBackEndLogin: 'http://gestion-des-absences.herokuapp.com/login/'
+  urlBackEndAbsences: 'https://gda-absences.herokuapp.com/absences/',
+  urlBackEndAbsencesStatut: 'https://gda-absences.herokuapp.com/absences?statut=',
+  urlBackEndJoursFeries: 'https://gda-absences.herokuapp.com/jours_feries/',
+  urlBackEndLogin: 'https://gda-absences.herokuapp.com/login'
 };
